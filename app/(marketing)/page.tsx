e2dad85c9c04bce15b26ada1014361bf0dc78c1b
Ananya Sikdar <iamananyasikdar@gmail.com>
2024-12-26 22:35:37 +0530

    marketing sketch

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
  return (
-  <Button size="lg">Click me</Button>
+//   <Button size="lg">Click me</Button>
+<div>This is a marketing page</div>
  )
 }
